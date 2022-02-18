ce7662403d40632dc59e1d1b3ca726638b1770bf
Farcas Andreea <farcasandreea98@gmail.com>
2022-02-18 17:39:11 +0200

    First Version

---
+++ script.js
@@ -0,0 +1,26 @@
+function randomNumber(n) {
+	return Math.floor(Math.random() * n + 1);
+}
+
+function generateButtons() {
+	let inputNumber = document.getElementById("inputNumber").value;
+	let winnerButton = randomNumber(inputNumber);
+
+	for(let i = 1; i <= inputNumber; i++) {
+		let btn = document.createElement("button");
+		btn.innerHTML = "Button " + i;
+     	btn.onclick = function() {
+     		let result = document.getElementById("result");
+    		if (i == winnerButton) {
+      			result.innerHTML ="You won. Lucky you!"
+      		} else {
+      			result.innerHTML ="You lost. Not your lucky day";
+      		}
+    	}
+    	document.getElementById("buttons").append(btn);
+  	} 
+}
+
+
+
+
